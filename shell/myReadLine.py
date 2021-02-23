41acc0818dbd94f58c19234121c162381f095e6a
LailaMRomero <lmromero4@miners.utep.edu>
2021-02-22 17:11:53 -0700

    updated

--- shell/myReadLine.py
+++ shell/myReadLine.py
@@ -2,25 +2,27 @@
 
 
 
+ #! /usr/bin/env python3
+
 from os import read
 
-ibuf = ""      # Input buffer
+ibuf = ""      # Input buffer, 
 sbuf = ""      # String buffer
 sbufLength = 0 # String buffer length
 currChar = 0   # Index of current char in sbuf
 
-def my_getChar():
+def getChar():
     global ibuf
     global sbuf
     global sbufLength
     global currChar
     
     if currChar == sbufLength: # If we reached the end of sbuf, get a new line and reset values
-        ibuf = read(0, 100)
+        ibuf = read(0, 100) # The number of bytes that can be accepted
         sbuf = ibuf.decode()
-        sbufLength = len(sbuf)
+        sbufLength = len(sbuf) # The length of the string
         currChar = 0
-        if sbufLength == 0:    # If we reached the end of the input, return nothing
+        if sbufLength == 0:    # If we reached the end of the input then it would return nothing
             return ''
     
     char = sbuf[currChar]
@@ -28,12 +30,12 @@ def my_getChar():
     return char
 
 def readLine():
-    char = my_getChar()
+    char = getChar()
     line = ""
     
-    while char != '\n':     # While char isnt equal to new line, keep getting chars for line
+    while char != '\n':     # While char is not equal to new line, keep getting chars for line
         line += char
-        char = my_getChar()
+        char = getChar()
         if char == '':      # If char is empty, then we reached EOF; retun
             return line
     line+= '\n'             # If a new line was found, then return the line with a new line char
@@ -43,6 +45,5 @@ def main():
     readLine()
     
 if '__main__' == __name__:
-    main()
+    main() 
     
-
