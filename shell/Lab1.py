e3f53c6f0fd4b6221c71aa82d422b21f03284e02
LailaMRomero <47288670+LailaMRomero@users.noreply.github.com>
2021-02-22 18:40:19 -0700

    Update Lab1.py
    updated

--- shell/Lab1.py
+++ shell/Lab1.py
@@ -1,66 +1,78 @@
 #! /usr/bin/env python3
 
-import os, sys, re #testing
-
+import os, sys, re
+from myreadLines import readLine
+from myRedirect import redirect
+from myPiping import pipeInput
 def main():
-    
-    while True: #trying to replicate what a shell does 
-        
+
+    while True:
         if 'PS1' in os.environ:
-            os.write(1, (os.environ['PS1']).encode())
+            os.write(1, ("$ ").encode())
         else:
             os.write(1, ("$ ").encode())
             
-        args = os.read(0, 1024) #make to  5 replace with myreadline
+        args = readLine() # We are using the readLine() found in the myReadLine.py
         
+        if '|' in args: #check for pipe here so we can split in pipe()
+            pipeInput(args)
+            continue
         if len(args) == 0:
-            break #exits while loop
+            break # This exits while loop
         
-        args = args.decode().split("\n") 
+        args = args.split("\n") 
         
         if not args:
-            continue # goes back to start of while loop
+            continue # This goes back to start of while loop
         
         for arg in args:
-            inputHandler(arg.split()) 
+            inputHandler(arg.split())
             
 def inputHandler(args):
-  
-    if len(args) == 0: # do nothing, return to main
+    if len(args) == 0: # Nothing really happens here so then it would return to main
         return
         
-    if "exit" in args: 
+    if "exit" in args: # So if I was to write exit, then the program would end
         sys.exit(0)
 
-    # change directory
+    # This is the change directory when used "cd"
     elif "cd" == args[0]:
         try:
-            if len(args)==1: # if just cd is specified, reprompt the user
+            if len(args)==1: # If cd is specified then it would reprompt the user
                 return   
             else:
                 os.chdir(args[1])
-        except: # nonexistent
-            os.write(1, ("cd %s: No such file or directory\n" % args[1]).encode())
+        except: # If the directory does not existent
+            os.write(1, ("cd %s: No such file or directory\n" % args[1]).encode())            
     else:
-        rc = os.fork()
+        rc = os.fork() # The purpose of forking is to create a new process ... (I'm gonna add more to this later)
         
         if rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
             sys.exit(1)
         elif rc == 0:
+            if ">" in args: # Check for redirect
+                redirect(args)
+            if "<" in args:
+                redirect(args)
+            
+            # These are meant for checking with direction inside the input
             executeCommand(args)
             sys.exit(0)
-
+            
 def executeCommand(args):
-    for dir in re.split(":", os.environ['PATH']):
+    for dir in re.split(":", os.environ['PATH']): # The path, takes in the path (checker)
         program = "%s/%s" % (dir, args[0])
         try:
-            os.execve(program, args, os.environ) # try to exec program
+            os.execve(program, args, os.environ) # This is where we execute the program
+            #what does
         except FileNotFoundError:
             pass
         
-    os.write(2, ("%s: command not found\n" % args[0]).encode()) # command not found, print an error message
+    os.write(1, ("%s: command not found\n" % args[0]).encode()) # If the command not found then we would print an error message
     sys.exit(0)
 
 if '__main__' == __name__:
     main() 
+    
+
