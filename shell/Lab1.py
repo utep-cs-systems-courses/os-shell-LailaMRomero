a852271ee6111c43d5e06bea8471f036fd19d164
LailaMRomero <lmromero4@miners.utep.edu>
2021-02-24 18:14:12 -0700

    updated files

--- shell/Lab1.py
+++ shell/Lab1.py
@@ -1,22 +1,24 @@
 #! /usr/bin/env python3
-#testing for updates
+#testing
 import os, sys, re
-from myreadLines import readLine
+from myReadLine import readLine
 from myRedirect import redirect
-from myPiping import pipeInput
-def main():
+#from myPiping import pipeInput
 
+def main():
+    '''This is trying to replicate what a shell actually does, but the main reason why we use a while is because we get to keep going and write other commands'''
     while True:
         if 'PS1' in os.environ:
-            os.write(1, ("$ ").encode())
+            os.write(1,("$ ").encode())
         else:
             os.write(1, ("$ ").encode())
             
         args = readLine() # We are using the readLine() found in the myReadLine.py
-        
+        #args = read(0, 1024)
         if '|' in args: #check for pipe here so we can split in pipe()
             pipeInput(args)
             continue
+        
         if len(args) == 0:
             break # This exits while loop
         
@@ -45,34 +47,65 @@ def inputHandler(args):
         except: # If the directory does not existent
             os.write(1, ("cd %s: No such file or directory\n" % args[1]).encode())            
     else:
-        rc = os.fork() # The purpose of forking is to create a new process ... (I'm gonna add more to this later)
+        rc = os.fork() # The purpose of forking is to create a new process 
         
         if rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
             sys.exit(1)
         elif rc == 0:
-            if ">" in args: # Check for redirect
-                redirect(args)
-            if "<" in args:
-                redirect(args)
+           # if ">" in args: # Check for redirect
+            #    redirect(args)
+            #if "<" in args:
+             #   redirect(args)
             
             # These are meant for checking with direction inside the input
             executeCommand(args)
             sys.exit(0)
             
 def executeCommand(args):
-    for dir in re.split(":", os.environ['PATH']): # The path, takes in the path (checker)
-        program = "%s/%s" % (dir, args[0])
+    if "/" in args[0]:#if / found in argument then do the following
+        program=args[0]#puts argument 0 into the value called program
         try:
-            os.execve(program, args, os.environ) # This is where we execute the program
-            #what does
+            os.execve(program,args,os.environ)#execute a process with enviornment in mind
         except FileNotFoundError:
             pass
-        
-    os.write(1, ("%s: command not found\n" % args[0]).encode()) # If the command not found then we would print an error message
+    elif ">" in args or "<" in args:#This is for if there's redirection in the argument
+        redirect(args)#if there is redirection go to redirection with arguments
+    else:
+        for dir in re.split(":", os.environ['PATH']):#breaks the path apart by ppattern of : in the environment variable path
+            program = "%s/%s" % (dir, args[0])#passes dir into first % to set up the file path then puts the first word in teh argument into the second %
+            try:
+                os.execve(program, args, os.environ)#tries to execute with given the parameters of program being the path, args being the argumetns and os.environ being the enviornment
+            except FileNotFoundError:
+                pass
+    os.write(2, ("%s: command not found\n" % args[0]).encode())#error code 
     sys.exit(0)
+
+def pipeInput(args):#the pipes method that take in arguments
+    left=args[0:args.index("|")]# gets data of left side of arguments before |
+    right=args[len(left)+1:]#gets the data of right side of arguments after |
+    pRead, pWrite = os.pipe()#making the read and write 
+    rc=os.fork()##creates a child process
+    if rc<0:# if the returns a 0 the for failed
+        os.write(2, ("Fork has failed returning now %d\n" %rc).encode())#
+        sys.exit(1)# used to exit
+    elif rc==0:#if return value is 0 do the following
+        os.close(1)#close file descriptor 1
+        os.dup(pWrite)#copies the file descriptors of the child and puts it into pWrite
+        os.set_inheritable(1,True)#
+        for fd in (pRead,pWrite):
+            os.close(fd)#closes all the file descriptors
+        executeCommand(left)#inputs the left argument into commands
+    else:
+        os.close(0)#closes file descriptor 0
+        os.dup(pRead)#copies the files descriptor of the parent and puts it into pRead
+        os.set_inheritable(0,True)#
+        for fd in (pWrite, pRead):
+            os.close(fd)#closes file descriptors in both pRead,pWrite
+        if "|" in right:#if it finds '|' on the right side of argument then it's piping it with right's varaibles
+            pipe(right)#goes into pipe with variable pipe
+        executeCommand(right)#inputs the right argument into commands
 
 if '__main__' == __name__:
     main() 
     
-
